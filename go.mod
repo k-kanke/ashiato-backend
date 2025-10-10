120d139be0da3900f516e93964451b0f16bcaf78
Kotaro Kanke <kanke.kooooo01@gmail.com>
2025-10-10 21:46:20 +0900

    fix

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/k-kanke/ashiato-backend
+
+go 1.21
---
+++ pkg/repository/pin_repository.go
@@ -0,0 +1,18 @@
+package repository
+
+import "github.com/k-kanke/ashiato-backend/pkg/domain"
+
+type PinRepository interface {
+	// ピンを作成
+	CreatePin(pin *domain.Pin) error
+
+	// 特定の矩形範囲内のPin情報を取得する
+	GetPinsInArea(
+		userID string,
+		minLat, maxLat, minLng, maxLng float64,
+		privacySetting string,
+	) ([]domain.Pin, error)
+
+	// Pinにコメントを追加する
+	CreateComment(comment *domain.Comment) error
+}
