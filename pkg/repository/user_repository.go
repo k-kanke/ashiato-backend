ae4f2b7bbc63df4be2cd7a30a96daa816876dd1f
Kotaro Kanke <kanke.kooooo01@gmail.com>
2025-10-11 11:07:48 +0900

    feat: user_repository

---
+++ pkg/repository/user_repository.go
@@ -0,0 +1,19 @@
+package repository
+
+import "github.com/k-kanke/ashiato-backend/pkg/domain"
+
+type UserRepository interface {
+	// ユーザーアカウントを新規作成する
+	CreateUser(
+		user *domain.User,
+		setting *domain.UserSettings,
+	) error
+
+	// メールアドレスを基にユーザーを検索する
+	FindUserByEmail(email string) (*domain.User, error)
+
+	// UserIDを基にユーザーと設定を検索する
+	FindUserByID(usreID string) (*domain.User, *domain.UserSettings, error)
+
+	// その他のフレンドや設定更新メソッド
+}
