7d472cf1ac0407d51102f3b8bb0b48965140a1f9
Kotaro Kanke <kanke.kooooo01@gmail.com>
2025-10-11 11:21:42 +0900

    DB接続設定

---
+++ pkg/infra/database/client.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type DBClient struct {
+	DB *sql.DB
+}
+
+func NewDBClient(dsn string) (*DBClient, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	// 必要に応じて接続プール設定
+
+	return &DBClient{DB: db}, nil
+}
