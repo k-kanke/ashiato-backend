60a77837df18d7a6d9bb20851a38120e59ecc9d7
Kotaro Kanke <kanke.kooooo01@gmail.com>
2025-10-13 16:10:41 +0900

    cors設定

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/k-kanke/ashiato-backend/pkg/api/handler"
 	"github.com/k-kanke/ashiato-backend/pkg/api/middleware"
@@ -12,6 +15,15 @@ func SetupRouter(
 	friendHandler *handler.FriendHandler,
 ) *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3001"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	v1 := router.Group("/v1")
 	{
