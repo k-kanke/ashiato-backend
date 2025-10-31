fb215b1eff73aeba0d166c5d8e837311b7e79a94
Kotaro Kanke <kanke.kooooo01@gmail.com>
2025-10-31 21:58:00 +0900

    fix

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -20,7 +20,7 @@ func SetupRouter(
 	router.Static("/uploads", "./uploads")
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3001"},
+		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
